0066100181d6439815299c2481a57bbb6b8e2798
Jiracet <jsuh96@gmail.com>
2015-02-13 17:05:42 -0500

    transfer to unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public string keyUp;
+	public string keyDown;
+	public string keyRight;
+	public string keyLeft;
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (keyUp)) {
+			this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + speed);
+		} else if (Input.GetKey (keyDown)) {
+			this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - speed);
+		}
+
+		if (Input.GetKey (keyRight)) {
+			this.transform.position = new Vector2(this.transform.position.x + speed, this.transform.position.y);
+		} else if (Input.GetKey (keyLeft)) {
+			this.transform.position = new Vector2(this.transform.position.x - speed, this.transform.position.y);
+		}
+	}
+}
